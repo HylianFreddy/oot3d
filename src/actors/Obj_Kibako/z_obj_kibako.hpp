fe5c7188942923aad30caacd71d5165b73897f93
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2022-08-15 22:15:42 +0200

    ObjKibako basic struct

--- src/actors/Obj_Kibako/z_obj_kibako.hpp
+++ src/actors/Obj_Kibako/z_obj_kibako.hpp
@@ -5,9 +5,15 @@
 
 struct ObjKibako;
 
+typedef void (*ObjKibakoActionFunc)(struct ObjKibako*, PlayState*);
+
 typedef struct ObjKibako {
-    /* 0x0000 */ Actor actor;
-    /* 0x01A4 */ char unk_1A4[0x68];
+    /* 0x000 */ Actor actor;
+    /* 0x1A4 */ ObjKibakoActionFunc actionFunc;
+    /* 0x1A8 */ ColliderCylinder collider;
+    /* 0x200 */ SkeletonAnimationModel* skelAnimModel;
+    /* 0x204 */ u32 unk_204;
+    /* 0x208 */ u32 unk_208;
 } ObjKibako; // size = 0x20C
 
 extern ActorInit Obj_Kibako_InitVars;
